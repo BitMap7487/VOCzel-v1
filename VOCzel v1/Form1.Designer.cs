c659b8b4e2f5c165151650b8aab9e36ea93002ae
BryanPiek03 <45109007+BryanPiek03@users.noreply.github.com>
2022-11-20 15:37:06 +0100

    Add project files.

---
+++ VOCzel v1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace VOCzel_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.commandCount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AllCommands = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.AllCommands, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.commandCount);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 444);
+            this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(190, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Loaded Commands:";
+            // 
+            // commandCount
+            // 
+            this.commandCount.AutoSize = true;
+            this.commandCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandCount.Location = new System.Drawing.Point(9, 311);
+            this.commandCount.Name = "commandCount";
+            this.commandCount.Size = new System.Drawing.Size(23, 25);
+            this.commandCount.TabIndex = 10;
+            this.commandCount.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Result:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Live Speech:";
+            // 
+            // AllCommands
+            // 
+            this.AllCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AllCommands.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AllCommands.Location = new System.Drawing.Point(403, 3);
+            this.AllCommands.Name = "AllCommands";
+            this.AllCommands.ReadOnly = true;
+            this.AllCommands.Size = new System.Drawing.Size(394, 444);
+            this.AllCommands.TabIndex = 1;
+            this.AllCommands.Text = "";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "VOCZEL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label commandCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox AllCommands;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ VOCzel v1/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Threading;
+
+using CSScriptLib;
+using System.Net;
+
+namespace VOCzel_v1
+{
+
+    public partial class Form1 : Form
+    {
+
+        public static List<string> LoadedCommands = new List<string>();
+
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+
+        // SpeechRecognition
+        SpeechRecognitionEngine defaultCommands = new SpeechRecognitionEngine();
+
+        // SpeechSynthesizer
+        static SpeechSynthesizer speech = new SpeechSynthesizer();
+
+        // SpeechRecognition when Start listening 
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        static Choices choices = new Choices();
+
+        public static string name = Environment.UserName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetupCheck();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadCommands();
+            AllCommands.Text = string.Join("\n", LoadedCommands);
+            commandCount.Text = LoadedCommands.Count.ToString();
+            // Start _recognizer
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(choices));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recEngine_StartRecognizion);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            // Does Commands once KEYWORD is said
+            defaultCommands.SetInputToDefaultAudioDevice();
+            defaultCommands.LoadGrammarAsync(new Grammar(choices));
+            defaultCommands.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recEngine_SecondRecognizion);
+
+        }
+
+        private void recEngine_StartRecognizion(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            string result = e.Result.Text;
+
+            if (result == "Voczel")
+            {
+
+                result = $"Hello {name}.";
+                _recognizer.RecognizeAsyncCancel();
+                defaultCommands.RecognizeAsync(RecognizeMode.Multiple);
+                // Stop Recognition
+
+                speech.SpeakAsync(result);
+                label4.Text = result;
+
+            }
+
+            
+
+        }
+
+        private void recEngine_SecondRecognizion(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            string result = e.Result.Text;
+
+            if (result != "Voczel")
+            {
+
+                foreach (string commandFile in Directory.GetFiles(Environment.CurrentDirectory + "//Commands"))
+                {
+
+                    if (commandFile.Contains(".cs"))
+                    {
+                        string FirstParse = commandFile.Split('\\').Last();
+                        string commandName = FirstParse.Split('.').First();
+
+                        if (result == commandName)
+                        {
+                            result = $"Running {commandName}";
+
+                            speech.SpeakAsync(result);
+                            label4.Text = result;
+
+                            string commandPath = commandFile.Replace("//", "\\");
+
+                            string command = File.ReadAllText(commandPath);
+
+                            Execute(command);
+
+                        }
+                    }
+
+                }
+
+            }
+            if(result == "Stop Listening")
+            {
+                result = $"No longer listening";
+                speech.SpeakAsync(result);
+                label4.Text = result;
+
+                restartListening();
+
+            }
+            if (result == "Shutdown")
+            {
+                result = $"Goodbye {name}";
+                speech.SpeakAsync(result);
+                label4.Text = result;
+                Thread.Sleep(1000);
+                Application.Exit();
+
+            }
+            if (result == "Mute")
+            {
+                    speech.SpeakAsync("As you wish " + name);
+                    System.Threading.Thread.Sleep(1000);
+                    speech.Volume = 0;
+                    label4.Text = "As you wish " + name;
+
+            }
+            if (result == "Unmute")
+            {
+                speech.Volume = 100;
+                speech.SpeakAsync("As you wish " + name);
+                label4.Text = "As you wish " + name;
+
+            }
+            if (result == "Reload Commands")
+            {
+
+                loadCommands();
+                speech.SpeakAsync("As you wish " + name);
+                label4.Text = "As you wish " + name;
+
+                AllCommands.Clear();
+
+                AllCommands.Text = string.Join("\n", LoadedCommands);
+                commandCount.Text = LoadedCommands.Count.ToString();
+
+            }
+            if(result == "Hide")
+            {
+                this.Hide();
+                speech.SpeakAsync("As you wish " + name);
+                notifyIcon.Visible = true;
+            }
+            if(result == "Unhide")
+            {
+                this.Show();
+                speech.SpeakAsync("As you wish " + name);
+                notifyIcon.Visible = false;
+            }
+
+
+
+            restartListening();
+
+        }
+
+        private void restartListening()
+        {
+            try
+            {
+                defaultCommands.RecognizeAsyncStop();
+                defaultCommands.RecognizeAsyncCancel();
+
+                _recognizer.RecognizeAsyncStop();
+                _recognizer.RecognizeAsyncCancel();
+                Thread.Sleep(1000);
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show("Something failed: " + ex);
+
+                defaultCommands.RecognizeAsyncStop();
+                defaultCommands.RecognizeAsyncCancel();
+
+                _recognizer.RecognizeAsyncStop();
+                _recognizer.RecognizeAsyncCancel();
+                Thread.Sleep(1000);
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void SetupCheck()
+        {
+            
+            if(!Directory.Exists(Environment.CurrentDirectory + "//Commands")){
+                Directory.CreateDirectory(Environment.CurrentDirectory + "//Commands");
+
+                WebClient client = new WebClient();
+                string reply = client.DownloadString("https://pastebin.com/raw/5kN9ZfVD");
+
+                using (TextWriter tw = new StreamWriter(Environment.CurrentDirectory + "//Commands/example.cs"))
+                {
+
+                    tw.Write(reply);
+
+                }
+
+            }
+
+        }
+
+        private void loadCommands()
+        {
+
+            LoadedCommands.Clear();
+            defaultCommands.UnloadAllGrammars();
+
+            string prefix = "Voczel";
+            string stopListenCMD = "Stop Listening";
+
+            string exitProgramCMD = "Shutdown";
+            string Mute = "Mute";
+            string Unmute = "Unmute";
+            string reloadCommands = "Reload Commands";
+
+            string Hide = "Hide";
+            string unHide = "Unhide";
+
+            choices.Add(prefix);
+            choices.Add(stopListenCMD);
+            choices.Add(exitProgramCMD);
+            choices.Add(Mute);
+            choices.Add(Unmute);
+            choices.Add(reloadCommands);
+            choices.Add(Hide);
+            choices.Add(unHide);
+
+            //Load all other Custom Commands
+            foreach (string commandFile in Directory.GetFiles(Environment.CurrentDirectory + "//Commands"))
+            {
+
+                if (commandFile.Contains(".cs"))
+                {
+                    string FirstParse = commandFile.Split('\\').Last();
+                    string commandName = FirstParse.Split('.').First();
+
+                    choices.Add(commandName);
+
+                    LoadedCommands.Add(commandName);
+
+                }
+
+            }
+
+            defaultCommands.LoadGrammarAsync(new Grammar(choices));
+
+        }
+
+        public static void Execute(string code)
+        {
+            try
+            {
+                dynamic block = CSScript.Evaluator
+                               .ReferenceAssembliesFromCode(code)
+                               .LoadCode(code);
+                block.ExecuteAFunction();
+            }
+            catch(Exception ex)
+            {
+                speech.SpeakAsync("Failed to run command.");
+                Clipboard.SetText(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        
+
+    }
+}
